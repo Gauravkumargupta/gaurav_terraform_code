4bdcd0e1376f800b7ba8dd8e17b66a3bb1720e3b
Gaurav <kumargaurav1247@gmail.com>
2020-01-12 03:23:25 +0530

    Change

--- 21days_Terraform/variables.tf
+++ 21days_Terraform/variables.tf
@@ -12,14 +12,16 @@ variable "private_cidrs" {
   default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
-variable "alarms_email" {
+variable "alarms_email" {}
 
-}
+variable "sns_topic" {}
 
-variable "sns_topic" {
+variable "instance_id" {}
 
-}
+variable "db_instance" {}
 
-variable "instance_id" {
+variable "rds_subnet1" {}
 
-}
+variable "rds_subnet2" {}
+
+variable "vpc_id" {}
